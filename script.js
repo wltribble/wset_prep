374372f7d4f8225bf97085ec714a22045493f1dd
wltribble <wltribble@gmail.com>
2025-04-03 14:06:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -93,11 +93,11 @@ function checkAnswer(selected, question) {
   const feedback = document.getElementById('feedback');
   totalAnswered++;
   if (selected === question.answer) {
-    feedback.textContent = '\n✅ Correct! ' + question.explanation;
+    feedback.textContent = '✅ Correct! ' + question.explanation + '<br.';
     feedback.className = 'correct';
     totalCorrect++;
   } else {
-    feedback.textContent = `\n❌ Incorrect. Correct answer: ${question.answer}. ` + question.explanation;
+    feedback.textContent = `❌ Incorrect. Correct answer: ${question.answer}. ` + question.explanation + '<br.';
     feedback.className = 'incorrect';
     missed.push(question);
     localStorage.setItem('missedQuestions', JSON.stringify(missed));
